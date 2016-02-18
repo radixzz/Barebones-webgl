11ad63c45ac7fd8b2d811ce2bf8b0abb158493c0
Iván Juárez <ivan@blindgazer.com>
2016-02-18 01:49:09 -0600

    Embedding shaders into the index.html and fixing shader creation.

--- initialization/main.js
+++ initialization/main.js
@@ -1,32 +1,12 @@
-
-var XHRLoad = function( url, callback ) {
-	var d = Q.defer();
-	var request = new XMLHttpRequest();
-	request.open( 'GET', url, true );
-	request.addEventListener( 'load', function() {
-		d.resolve( request.responseText );
-	} );
-	request.send();
-	return d.promise;
-};
-
-var loadShaders = function( renderer ) {
-	
-	var xhrJob = [ 
-		XHRLoad('vertex.shader'),
-		XHRLoad('fragment.shader')
-	];
-
-	Q.when( xhrJob ).then( function( vShader, fShader ) {
-		var shader = new WebGL.Shader( vShader, fShader );
-		renderer.addShader( shader );
-	});	
-}
-
 var initDemo = function() {
 	var renderer = new WebGL.Renderer( 500, 500 );
 	document.body.appendChild( renderer.domElement );
-	loadShaders( renderer );
+
+	var fShader = document.getElementById('shader-fs').textContent;
+	var vShader = document.getElementById('shader-vs').textContent;
+	var shader = new WebGL.Shader( vShader, fShader  );
+
+	renderer.addShader( shader );
 	renderer.setClearColor( 1, 0, 0, 1 );
 	renderer.clear();
 	renderer.render();
--- initialization/vertex.shader
+++
@@ -1,4 +0,0 @@
-attribute vec2 vertexCoord;
-void main(void) {
-    gl_Position = vec4(vertexCoord, 0, 1);
-}
